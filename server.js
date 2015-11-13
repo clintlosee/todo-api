336df57f0448d1c6d6c113dca966b4617ac94cd5
Clint Losee <clint.losee@gmail.com>
2015-11-13 09:20:15 -0700

    Changed port variable

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express'),
     app     = express(),
-    PORT    = process.env.port || 8000;
+    port    = process.env.port || 8000;
 
 app.get('/', function(req, res) {
     res.send('Todo API Root');
 });
 
-app.listen(PORT, function() {
-    console.log('Magic happens on port ' + PORT);
+app.listen(port, function() {
+    console.log('Magic happens on port ' + port);
 });
