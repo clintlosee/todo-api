ffc6f9e698f5df6ad00a37b7495a70c3db812a6f
Clint Losee <clint.losee@gmail.com>
2015-11-16 08:34:38 -0700

    Adding POST ability for new todo items

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express'),
     app     = express(),
+    bodyParser = require('body-parser'),
     port    = process.env.PORT || 8000,
     todos   = [{
         id: 1,
@@ -11,33 +12,53 @@ var express = require('express'),
         completed: false
     }, {
         id: 3,
-        description: 'Eat dinner',
+        description: 'Eat lunch',
         completed: true
-    }];
+    }],
+    todosArr = [],
+    todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
     res.send('Todo API Root');
 });
 
 app.get('/todos', function(req, res) {
-    res.json(todos);
+    res.json(todosArr);
 });
 
 app.get('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10),
         match;
 
-    todos.forEach(function(todo) {
-        if (todo.id === todoId) {
-            match = todo;
+    // SLOWER
+    // todos.forEach(function(todo) {
+    //     if (todo.id === todoId) {
+    //         match = todo;
+    //     }
+    // });
+
+    for (var i = 0; i < todosArr.length; i++) {
+        if (todosArr[i].id === todoId) {
+            match = todosArr[i];
         }
-    });
+    }
 
     if (match) {
         res.json(match);
     } else {
         res.status(404).send();
     }
+});
+
+app.post('/todos', function(req, res) {
+    var body = req.body;
+
+    body.id = todoNextId++;
+    todosArr.push(body);
+
+    res.json(body);
 });
 
 app.listen(port, function() {
