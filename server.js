6e00cf355b6deba9f4e4a62e2efda2b88c85f549
Clint Losee <clint.losee@gmail.com>
2015-11-26 15:00:47 -0700

    Initial setup for user login requests.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express     = require('express'),
     bodyParser  = require('body-parser'),
-    _           = require('underscore'),
+    _           = require('underscore')
+	bcrypt		= require('bcrypt'),
     db          = require('./db.js');
 
 var app         = express(),
@@ -132,6 +133,30 @@ app.post('/users', function(req, res) {
         res.status(400).json(e);
     });
 });
+
+// POST /users/login
+app.post('/users/login', function(req, res) {
+	var body = _.pick(req.body, 'email', 'password');
+	
+	if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+		return res.status(400).send();
+	}
+	
+	db.user.findOne({
+		where: {
+			email: body.email
+		}
+	}).then(function(user) {
+		if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+			return res.status(401).send();
+		}
+		
+		res.json(user.toPublicJSON());
+	}, function(e) {
+		res.status(500).send();
+	});
+});
+
 
 db.sequelize.sync().then(function() {
     app.listen(port, function() {
